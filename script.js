06579bd652bd830876b4994bdb3f37c0df0e91f4
sinkolinko <leoliker@gmail.com>
2022-08-05 22:49:13 -0400

    Refactore point selector code

--- script.js
+++ script.js
@@ -1,158 +1,50 @@
 'use strict';
 
-// console.log('open');
-
-// Setting the Id and Elements
-const player22 = document.getElementById('player-22');
-const player15 = document.getElementById('player-15');
-const player4 = document.getElementById('player-4');
-const player51 = document.getElementById('player-51');
-const player13 = document.getElementById('player-13');
-
-const point1 = document.getElementById('point-1');
-const point2 = document.getElementById('point-2');
-const point3 = document.getElementById('point-3');
-
-const player22Points = document.getElementById('player-22-points');
-const player15Points = document.getElementById('player-15-points');
-const player4Points = document.getElementById('player-4-points');
-const player51Points = document.getElementById('player-51-points');
-const player13Points = document.getElementById('player-13-points');
-
-let player22Score = 0;
-let player15Score = 0;
-let player4Score = 0;
-let player51Score = 0;
-let player13Score = 0;
-
-function clearPlayers () {
-  player22.classList.remove('selected');
-  player15.classList.remove('selected');
-  player4.classList.remove('selected');
-  player51.classList.remove('selected');
-  player13.classList.remove('selected');
+// Clear player selection
+function clearPlayers() {
+  document.querySelectorAll('.player').forEach(item => {
+    item.classList.remove('selected');
+    console.log('Players Cleared');
+  })
 }
 
-function clearPoints () {
-  point1.classList.remove('selected');
-  point2.classList.remove('selected');
-  point3.classList.remove('selected');
+// Clear points selection
+function clearPoints() {
+  document.querySelectorAll('.point').forEach(item => {
+    item.classList.remove('selected');
+    console.log('points Cleared');
+  })
 }
-
-// chosenPlayer = chosenplayer === 22 ? 15 ? 4 ? 51 ? 13 : 22
-
-// Selecting the players
-player22.addEventListener('click', function() {
-  clearPlayers();
-  this.classList.add('selected');
-})
   
-player15.addEventListener('click', function() {
-  clearPlayers();
-  this.classList.add('selected');
+// Player selection
+let uid = 0;
+document.querySelectorAll('.player').forEach(item => {
+  item.addEventListener('click', function () {
+    clearPlayers();
+    // Highlighting the selected player
+    this.classList.add('selected');
+
+    // Get player uid
+    uid = this.querySelector('.uid');
+    let uidNumber = uid.textContent;
+    console.log(uidNumber);
+  })
 })
 
-player4.addEventListener('click', function() {
-  clearPlayers();
-  this.classList.add('selected');
+// Selecting points
+document.querySelectorAll('.point').forEach(item => {
+  item.addEventListener('click', function () {
+    clearPoints();
+    this.classList.add('selected');
+    const pointsID = item.getAttribute('id');
+    // 1 point clicked
+    if(pointsID === 'point-1') {
+      let pointsSelector = document.querySelector(`#player-${uid.textContent}-points`).textContent;
+      let currentPoints = Number(pointsSelector);
+      currentPoints = currentPoints + 1;
+      document.querySelector(`#player-${uid.textContent}-points`).textContent = currentPoints;
+      console.log('Point 1 clicked and player', uid.textContent, 'selected');
+      console.log(`Current points are ${currentPoints}`);
+    }
+  })
 })
-
-player51.addEventListener('click', function() {
-  clearPlayers();
-  this.classList.add('selected');
-})
-
-player13.addEventListener('click', function() {
-  clearPlayers();
-  this.classList.add('selected');
-})
-
-// Selecting the points scored by the player
-
-point1.addEventListener('click', function() {
-  clearPoints();
-  this.classList.add('selected');
-  if (player22.classList.contains('selected') && point1.classList.contains('selected')) {
-    player22Score++;
-    player22Points.textContent = (`${player22Score} points`);
-  }
-
-  if (player15.classList.contains('selected') && point1.classList.contains('selected')) {
-    player15Score++;
-    player15Points.textContent = (`${player15Score} points`)
-  }
-
-  if (player4.classList.contains('selected') && point1.classList.contains('selected')) {
-    player4Score++;
-    player4Points.textContent =  (`${player4Score} points`)
-  }
-
-  if (player51.classList.contains('selected') && point1.classList.contains('selected')) {
-    player51Score++;
-    player51Points.textContent = (`${player51Score} points`);
-  }
-
-  if (player13.classList.contains('selected') && point1.classList.contains('selected')) {
-    player13Score++;
-    player13Points.textContent = (`${player13Score} points`);
-  }
-})
-
-point2.addEventListener('click', function() {
-  clearPoints();
-  this.classList.add('selected');
-  if (player22.classList.contains('selected') && point2.classList.contains('selected')) {
-    player22Score = player22Score + 2;
-    player22Points.textContent = (`${player22Score} points`);
-  }
-
-  if (player15.classList.contains('selected') && point2.classList.contains('selected')) {
-    player15Score = player15Score + 2;
-    player15Points.textContent = (`${player15Score} points`);
-  }
-
-  if (player4.classList.contains('selected') && point2.classList.contains('selected')) {
-    player4Score = player4Score + 2;
-    player4Points.textContent =  (`${player4Score} points`)
-  }
-
-  if (player51.classList.contains('selected') && point2.classList.contains('selected')) {
-    player51Score = player51Score + 2;
-    player51Points.textContent = (`${player51Score} points`);
-  }
-
-  if (player13.classList.contains('selected') && point2.classList.contains('selected')) {
-    player13Score = player13Score + 2;
-    player13Points.textContent = (`${player13Score} points`);
-  }
-})
-
-point3.addEventListener('click', function() {
-  clearPoints();
-  this.classList.add('selected');
-  if (player22.classList.contains('selected') && point3.classList.contains('selected')) {
-    player22Score = player22Score + 3;
-    player22Points.textContent = (`${player22Score} points`);
-  }
-
-  if (player15.classList.contains('selected') && point3.classList.contains('selected')) {
-    player15Score = player15Score + 3;
-    player15Points.textContent = (`${player15Score} points`)
-  }
-
-  if (player4.classList.contains('selected') && point3.classList.contains('selected')) {
-    player4Score = player4Score + 3;
-    player4Points.textContent =  (`${player4Score} points`);
-  }
-
-  if (player51.classList.contains('selected') && point3.classList.contains('selected')) {
-    player51Score = player51Score + 3;
-    player51Points.textContent = (`${player51Score} points`);
-  }
-
-  if (player13.classList.contains('selected') && point3.classList.contains('selected')) {
-    player13Score = player13Score + 3;
-    player13Points.textContent = (`${player13Score} points`);
-  }
-})
-
