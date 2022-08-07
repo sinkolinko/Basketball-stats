a8f902de1a4e43470b455b42456142d15eaaa6da
giulianoliker <giulianoliker@gmail.com>
2022-08-07 01:40:44 -0400

    Firebase saving code updated Firebase unique ID is now time

--- README.md
+++ README.md
@@ -4,15 +4,10 @@ A simple web app we are building as we go through a Javascript course.
 
 TODO:
 
-- Store data locally
-  - EMCA Script time
-  - Team ID
-  - Player ID
-  - Action (points, fouls, etc)
+- Each player has to have unique ID independant from the jersey number
 - Add overtime time periods
 - Add fouls
-- Other stats...
 - Option to undo last action
-- Store data in cloud
-- Add points for the opponents
-- Add game score
+- Add total points for the opponents
+- ~~Add total score~~
+- ~~Store data to online database (Firebase: EMCA Script time, team ID, player ID, action)~~
--- script.js
+++ script.js
@@ -46,9 +46,11 @@ document.querySelectorAll(".player").forEach((item) => {
 // Points selection
 document.querySelectorAll(".point").forEach((item) => {
   item.addEventListener("click", function () {
+    
     this.classList.add("flash");
     setTimeout(clearPoints, 2000);
     const pointsID = item.getAttribute("id");
+
     // 1 point clicked
     if (pointsID === "point-1") {
       let pointsSelector = document.querySelector(
@@ -60,6 +62,7 @@ document.querySelectorAll(".point").forEach((item) => {
         currentPoints;
       addToTotal(1);
     }
+
     // 2 points clicked
     else if (pointsID === "point-2") {
       let pointsSelector = document.querySelector(
@@ -71,6 +74,7 @@ document.querySelectorAll(".point").forEach((item) => {
         currentPoints;
       addToTotal(2);
     }
+
     // 3 points clicked
     else if (pointsID === "point-3") {
       let pointsSelector = document.querySelector(
@@ -81,6 +85,15 @@ document.querySelectorAll(".point").forEach((item) => {
       document.querySelector(`#player-${uid.textContent}-points`).textContent =
         currentPoints;
       addToTotal(3);
+
+      // Save points to the database
+      console.log('3pt button clicked');
+      let currentTime = new Date().getTime();
+      firebase.database().ref('game-id/team-id/'+currentTime).set({
+        'player-id': 'Player unique ID',
+        'points': 'How many points scored',
+        'quarter': 'Current quarter'
+      });
     }
   });
 });
@@ -92,12 +105,3 @@ function addToTotal(howManyScored) {
   totalAdded = totalAdded + howManyScored;
   document.querySelector("#total-points").textContent = totalAdded;
 }
-
-// Firebase Testing
-var dbRef = firebase.database().ref('Test 01').child('Banana');
-
-// Using 3 Point button for testing purpose
-document.querySelector('#point-3').addEventListener('click', () => {
-  console.log('button clicked');
-  dbRef.push('This is a test 01');
-});
