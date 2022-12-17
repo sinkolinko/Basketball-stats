64126ae37f4acf11b154e6e90e1c405742549872
sinkolinko <leoliker@gmail.com>
2022-12-16 20:07:12 -0500

    Inline buttons implemented.

--- script.js
+++ script.js
@@ -7,17 +7,10 @@ function clearQuarters() {
   });
 }
 
-// Clear player selection
-function clearPlayers() {
-  document.querySelectorAll(".player").forEach((item) => {
-    item.classList.remove("selected");
-  });
-}
-
 // Clear points selection
 function clearPoints() {
   document.querySelectorAll(".point").forEach((item) => {
-    item.classList.remove("flash");
+    item.classList.remove("selected");
   });
 }
 
@@ -29,71 +22,53 @@ document.querySelectorAll(".quarter").forEach((item) => {
   });
 });
 
-// Player selection
-let uid = 0;
-document.querySelectorAll(".player").forEach((item) => {
-  item.addEventListener("click", function () {
-    clearPlayers();
-    // Highlighting the selected player
-    this.classList.add("selected");
-
-    // Get player uid
-    uid = this.querySelector(".uid");
-    let uidNumber = uid.textContent;
-  });
-});
-
 // Points selection
 document.querySelectorAll(".point").forEach((item) => {
   item.addEventListener("click", function () {
-    
-    this.classList.add("flash");
-    setTimeout(clearPoints, 2000);
-    const pointsID = item.getAttribute("id");
+    this.classList.add("selected");
+    setTimeout(clearPoints, 500);
+    const pointsID = item.getAttribute("data-action");
 
     // 1 point clicked
     if (pointsID === "point-1") {
-      let pointsSelector = document.querySelector(
-        `#player-${uid.textContent}-points`
-      ).textContent;
-      let currentPoints = Number(pointsSelector);
+      let currentPointsElement =
+        item.parentNode.querySelector(".player-points");
+      let currentPoints = Number(currentPointsElement.textContent);
       currentPoints = currentPoints + 1;
-      document.querySelector(`#player-${uid.textContent}-points`).textContent =
-        currentPoints;
+      currentPointsElement.textContent = currentPoints;
       addToTotal(1);
     }
 
     // 2 points clicked
     else if (pointsID === "point-2") {
-      let pointsSelector = document.querySelector(
-        `#player-${uid.textContent}-points`
-      ).textContent;
-      let currentPoints = Number(pointsSelector);
+      let currentPointsElement =
+        item.parentNode.querySelector(".player-points");
+      let currentPoints = Number(currentPointsElement.textContent);
       currentPoints = currentPoints + 2;
-      document.querySelector(`#player-${uid.textContent}-points`).textContent =
-        currentPoints;
+      currentPointsElement.textContent = currentPoints;
       addToTotal(2);
     }
 
     // 3 points clicked
     else if (pointsID === "point-3") {
-      let pointsSelector = document.querySelector(
-        `#player-${uid.textContent}-points`
-      ).textContent;
-      let currentPoints = Number(pointsSelector);
+      let currentPointsElement =
+        item.parentNode.querySelector(".player-points");
+      let currentPoints = Number(currentPointsElement.textContent);
       currentPoints = currentPoints + 3;
-      document.querySelector(`#player-${uid.textContent}-points`).textContent =
-        currentPoints;
+      currentPointsElement.textContent = currentPoints;
       addToTotal(3);
 
       // Save points to the database
-      console.log('3pt button clicked');
-      let currentTime = new Date().getTime();
-      firebase.database().ref('game-id/team-id/'+currentTime).set({
-        'player-id': 'Player unique ID',
-        'points': 'How many points scored',
-        'quarter': 'Current quarter'
-      });
+      // console.log("3pt button clicked");
+      // let currentTime = new Date().getTime();
+      // firebase
+      //   .database()
+      //   .ref("game-id/team-id/" + currentTime)
+      //   .set({
+      //     "player-id": "Player unique ID",
+      //     points: "How many points scored",
+      //     quarter: "Current quarter",
+      //   });
     }
   });
 });
