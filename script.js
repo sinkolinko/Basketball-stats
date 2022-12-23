2cf08444367c292d7b5e6d663190876257fb6d4f
sinkolinko <leoliker@gmail.com>
2022-12-23 17:51:59 -0500

    Implemented limit to player fouls and added alert

--- script.js
+++ script.js
@@ -24,61 +24,77 @@ document.querySelectorAll(".quarter").forEach((item) => {
 
 // Points selection
 document.querySelectorAll(".point").forEach((item) => {
-  item.addEventListener("click", function () {
-    this.classList.add("selected");
-    setTimeout(clearPoints, 1000);
-    const pointsID = item.getAttribute("data-action");
+  item.addEventListener(
+    "click",
+    function () {
+      this.classList.add("selected");
+      setTimeout(clearPoints, 1000);
+      const pointsID = item.getAttribute("data-action");
 
-    // 1 point clicked
-    if (pointsID === "point-1") {
-      let currentPointsElement =
-        item.parentNode.querySelector(".player-points");
-      let currentPoints = Number(currentPointsElement.textContent);
-      currentPoints = currentPoints + 1;
-      currentPointsElement.textContent = currentPoints;
-      addToTotal(1);
+      // 1 point clicked
+      if (pointsID === "point-1") {
+        let currentPointsElement =
+          item.parentNode.querySelector(".player-points");
+        let currentPoints = Number(currentPointsElement.textContent);
+        currentPoints = currentPoints + 1;
+        currentPointsElement.textContent = currentPoints;
+        addToTotal(1);
+      }
+
+      // 2 points clicked
+      else if (pointsID === "point-2") {
+        let currentPointsElement =
+          item.parentNode.querySelector(".player-points");
+        let currentPoints = Number(currentPointsElement.textContent);
+        currentPoints = currentPoints + 2;
+        currentPointsElement.textContent = currentPoints;
+        addToTotal(2);
+      }
+
+      // 3 points clicked
+      else if (pointsID === "point-3") {
+        let currentPointsElement =
+          item.parentNode.querySelector(".player-points");
+        let currentPoints = Number(currentPointsElement.textContent);
+        currentPoints = currentPoints + 3;
+        currentPointsElement.textContent = currentPoints;
+        addToTotal(3);
+      }
+
+      // Fouls clicked
+      else if (pointsID === "foul") {
+        let currentFoulsElement =
+          item.parentNode.querySelector(".player-fouls");
+        let currentFouls = Number(currentFoulsElement.textContent);
+        if (currentFouls === 5) {
+          console.log("Player is already out");
+          console.log(currentFouls);
+        } else if (currentFouls === 4) {
+          currentFouls = currentFouls + 1;
+          currentFoulsElement.textContent = currentFouls;
+          console.log("Player is out");
+          item.parentNode.querySelector(".player-fouls").classList.add("alert");
+          console.log(currentFouls);
+        } else {
+          currentFouls = currentFouls + 1;
+          currentFoulsElement.textContent = currentFouls;
+          console.log(currentFouls);
+        }
+      }
     }
 
-    // 2 points clicked
-    else if (pointsID === "point-2") {
-      let currentPointsElement =
-        item.parentNode.querySelector(".player-points");
-      let currentPoints = Number(currentPointsElement.textContent);
-      currentPoints = currentPoints + 2;
-      currentPointsElement.textContent = currentPoints;
-      addToTotal(2);
-    }
-
-    // 3 points clicked
-    else if (pointsID === "point-3") {
-      let currentPointsElement =
-        item.parentNode.querySelector(".player-points");
-      let currentPoints = Number(currentPointsElement.textContent);
-      currentPoints = currentPoints + 3;
-      currentPointsElement.textContent = currentPoints;
-      addToTotal(3);
-    }
-
-    // Fouls clicked
-    else if (pointsID === "foul") {
-      let currentFoulsElement = item.parentNode.querySelector(".player-fouls");
-      let currentFouls = Number(currentFoulsElement.textContent);
-      currentFouls = currentFouls + 1;
-      currentFoulsElement.textContent = currentFouls;
-
-      // Save points to the database
-      // console.log("3pt button clicked");
-      // let currentTime = new Date().getTime();
-      // firebase
-      //   .database()
-      //   .ref("game-id/team-id/" + currentTime)
-      //   .set({
-      //     "player-id": "Player unique ID",
-      //     points: "How many points scored",
-      //     quarter: "Current quarter",
-      //   });
-    }
-  });
+    // Save points to the database
+    // console.log("3pt button clicked");
+    // let currentTime = new Date().getTime();
+    // firebase
+    //   .database()
+    //   .ref("game-id/team-id/" + currentTime)
+    //   .set({
+    //     "player-id": "Player unique ID",
+    //     points: "How many points scored",
+    //     quarter: "Current quarter",
+    //   });
+  );
 });
 
 // Total points
